4dbe9cc123f6a43e232e463309c786193438e3c2
takuya maeda <heavenlybigwednesday@gmail.com>
2020-12-01 23:45:07 +0900

    :new: create LogGroup

--- lib/aws-cdk-handson-stack.ts
+++ lib/aws-cdk-handson-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import {LogGroup, RetentionDays} from '@aws-cdk/aws-logs';
 
 export class AwsCdkHandsonStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const logGroup = new LogGroup(this, 'LogGroup', {
+        logGroupName: 'WebServerLogGroup',
+        retention: RetentionDays.ONE_YEAR,
+    });
   }
 }
